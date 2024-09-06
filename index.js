89a56acbc306995477b1d33b9eb06677ea90d98a
unknown <Asad>
2024-09-06 17:43:29 +0500

    f

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000
+
+// const User = require('./routes')
+
+
+const main = require('./config/db');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+main();
+
+app.use('/', require('./routes/index'));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
