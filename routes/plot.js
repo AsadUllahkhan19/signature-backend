22e1f56b81b7702194d15804b4202b1e03b5645b
salman-aly <alysalman15@gmail.com>
2024-10-10 18:43:14 +0500

    ..

--- routes/plot.js
+++ routes/plot.js
@@ -13,4 +13,13 @@ router.post("/add-plot", async (req, res) => {
   }
 });
 
+router.get("/get-plot", async (req, res) => {
+  try {
+    const result = await Coordinate.find({});
+    return res.status(200).send({ data: result, message: "successfull" });
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
+  }
+});
+
 module.exports = router;
